5cb6fa1f8b7a18150adc3f071a164ff50a5f7c76
julianjca <julianjca11@gmail.com>
2019-02-13 19:48:20 +0700

    add currency state

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,19 @@
 import React from 'react'
 
 class Home extends React.Component {
+  state = {
+    currencies = [
+      "CAD",
+      "IDR",
+      "GBP",
+      "CHF",
+      "SGD",
+      "INR",
+      "MYR",
+      "JPY",
+      "KRW"
+    ]
+  }
   render() {
     return (
       <div>Home Page</div>
