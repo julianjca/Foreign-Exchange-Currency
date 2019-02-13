de3a38444b8fcc557fe6f09b105b63ff4c882518
julianjca <julianjca11@gmail.com>
2019-02-13 21:05:40 +0700

    remove unecessary import on Card Component

--- components/CurrencyCard/index.js
+++ components/CurrencyCard/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import axios from 'axios';
 
-import { API_URL, currencyDetails } from '../../constants';
+import { currencyDetails } from '../../constants';
 import { Card } from './style';
 
 class CurrencyCard extends React.Component {
