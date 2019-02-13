7d423cb3a4a69ab4be2ab20e094bcfdb47bf266d
julianjca <julianjca11@gmail.com>
2019-02-13 21:02:00 +0700

    adding style to component

--- components/CurrencyCard/style.js
+++ components/CurrencyCard/style.js
@@ -4,13 +4,13 @@ export const Card = styled.div`
 display: flex;
 justify-content: center;
 align-items: center;
-width: 100%;
 flex-direction: column;
 border: 1px solid black;
-width: 40%;
+width: 30%;
 margin: 20px auto;
 padding: 1% 0;
 text-align: center;
+border-radius: 10px;
   h4 {
     margin : 5px auto;
     font-size: 12px;
