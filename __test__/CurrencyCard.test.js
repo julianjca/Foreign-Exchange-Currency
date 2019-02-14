1a15024eac258e1177409a87d6e6089268b7862d
julianjca <julianjca11@gmail.com>
2019-02-14 09:18:09 +0700

    adding test for CurrencyCard Component

---
+++ __test__/CurrencyCard.test.js
@@ -0,0 +1,31 @@
+import { shallow, mount } from 'enzyme';
+import { configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import CurrencyCard from '../components/CurrencyCard';
+import { currencyDetails } from '../constants';
+
+configure({ adapter: new Adapter() });
+
+let mockRates = {
+  CAD: 1.3243697479
+};
+
+const currentValue = 10;
+
+describe('when the mouse moves', () => {
+  const wrapper = shallow(<CurrencyCard
+    currency="CAD"
+    currentValue={currentValue}
+    rates={mockRates}
+  />);
+  console.log(wrapper.state().currencyRate,'state')
+
+  it('have currency details of Canadian Dollar', () => {
+    expect(wrapper.state('currencyDetails')).toEqual(currencyDetails.CAD);
+  });
+
+  it('have currency rate of 1.3243697479', () => {
+    expect(wrapper.state('currencyRate')).toEqual(mockRates.CAD);
+  });
+});
